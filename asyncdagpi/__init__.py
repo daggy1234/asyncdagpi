a2db46f937a3a3a5987ccfee5b525bc46d3a4974
Daggy1234 <arnav.jindal7@gmail.com>
2020-10-17 02:06:00 +0530

    v2.0.3

--- asyncdagpi/__init__.py
+++ asyncdagpi/__init__.py
@@ -14,7 +14,7 @@ __title__ = "asyncdagpi"
 __author__ = "Daggy1234"
 __license__ = "MIT"
 __copyright__ = "Copyright 2020 Daggy1234"
-__version__ = '2.0.2'
+__version__ = '2.0.3'
 
 from .http import HTTP
 from .image_features import ImageFeatures
