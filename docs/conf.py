3f203c6c6f8aba0aefbb24e0b4ac63d59af089aa
Arnav Jindal <arnav.jindal7@gmail.com>
2021-07-21 17:16:04 +0530

    regex update

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ pygments_style = 'sphinx'
 
 version = ''
 with open('../asyncdagpi/__init__.py') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+    version = re.search(r"^__version__:\s*str\s*=\s*['\"]([^'\"]*)['\"]", f.read(), re.MULTILINE).group(1)
 # The full version, including alpha/beta/rc tags.
 release = version
 # -- Options for HTML output -------------------------------------------------
